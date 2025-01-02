bf3af15b68dda1d9438a858634de4d0fd2bd42ce
akmlrfzn <akmalahong15@gmail.com>
2025-01-02 21:00:06 +0700

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<header id="header" class="header fixed-top d-flex align-items-center">
+    <div class="d-flex align-items-center justify-content-between">
+        <a href="index.php" class="logo d-flex align-items-center">
+            <img src="assets/img/logo.png" alt="Logo">
+            <span class="d-none d-lg-block">Nice Admin</span>
+        </a>
+        <i class="bi bi-list toggle-sidebar-btn"></i>
+    </div>
+    <div class="search-bar">
+        <form class="search-form d-flex align-items-center" method="GET" action="#">
+            <input type="text" name="query" placeholder="Search" title="Enter search keyword">
+            <button type="submit" title="Search"><i class="bi bi-search"></i></button>
+        </form>
+    </div>
+</header>
---
+++ includes/navbar.php
@@ -0,0 +1,21 @@
+<aside id="sidebar" class="sidebar">
+    <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+            <a class="nav-link " href="index.php">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="pages-profile.php">
+                <i class="bi bi-person"></i>
+                <span>Profile</span>
+            </a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-settings.php">
+            <i class="bi bi-gear"></i>
+            <span>Settings</span>
+        </a>
+    </ul>
+</aside>
