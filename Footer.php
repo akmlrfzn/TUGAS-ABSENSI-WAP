c555e47e7de84391f9f2707be7a3e4097ec5ebbc
akmlrfzn <akmalahong15@gmail.com>
2025-01-04 18:56:12 +0700

    Update Footer.php

--- Footer.php
+++ Footer.php
@@ -1,12 +1,13 @@
-<footer id="footer" class="footer">
+<!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
     <div class="copyright">
-        &copy; 2025 <strong><span>Nice Admin</span></strong>. All Rights Reserved.
+      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
     </div>
     <div class="credits">
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
     </div>
-</footer>
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-<script src="assets/js/main.js"></script>
-</body>
-</html>
+  </footer><!-- End Footer -->
